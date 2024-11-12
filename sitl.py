ed1026217ba871943e4d7cbff6c3b3adffe573b9
ahthae <122731557+ahthae@users.noreply.github.com>
2024-11-11 20:18:42 -0800

    Send units in MQTT message

--- sitl.py
+++ sitl.py
@@ -12,7 +12,15 @@ def simulate_valkyrie():
         s = (datestr[:-4]+datestr[-3:])
         return datetime.strptime(s, "%H:%M:%S:%f")
 
-    topics = ['temperature/injector', 'temperature/vent', 'temperature/combustion', '', 'pressure/injector', 'pressure/tank', 'pressure/feed',  'load_cell/1', 'load_cell/2']
+    topics = [['temperature/injector', 'F'],
+              ['temperature/vent', 'F'],
+              ['temperature/chamber', 'F'],
+              ['', ''],
+              ['pressure/injector', 'psi'],
+              ['pressure/tank', 'psi'],
+              ['pressure/feed', 'psi'],
+              ['load_cell/1', 'g'],
+              ['load_cell/2', 'g']]
 
     with open('data/Datafile_1.csv') as data_file:
         reader = csv.reader(data_file)
@@ -22,9 +30,18 @@ def simulate_valkyrie():
             ts = parse_date(row[0])
             print(sim_ts)
             msgs = []
-            for i in range(len(row)-1):
-                if topics[i] != '':
-                    msgs.append({'topic':topics[i], 'payload':json.dumps({'timestamp':sim_ts.timestamp(), 'value':row[i+1]})})
+            for i in range(0, len(row)-1):
+                topic = topics[i][0]
+                units = topics[i][1]
+                if topic != '':
+                    msgs.append({
+                        'topic': topic,
+                        'payload': json.dumps({
+                            'timestamp':sim_ts.timestamp(), 
+                            'units': units, 
+                            'value':row[i+1]
+                            })
+                        })
 
             publish.multiple(msgs)
             delta = parse_date(next_row[0])-ts
