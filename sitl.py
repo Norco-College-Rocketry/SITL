090393886d2cadcaf8cc943e31e8816ba40f6214
ahthae <122731557+ahthae@users.noreply.github.com>
2024-11-24 16:21:02 -0800

    Move Influx setup into main

--- sitl.py
+++ sitl.py
@@ -9,22 +9,7 @@ import influxdb_client
 from influxdb_client.client.write_api import SYNCHRONOUS
 from dotenv import load_dotenv
 
-load_dotenv()
-
-url = os.getenv('INFLUX_URL')
-bucket = os.getenv('INFLUX_BUCKET')
-org = os.getenv('INFLUX_ORG')
-token = os.getenv('INFLUX_TOKEN')
-measurement = 'sitl'
-
-client = influxdb_client.InfluxDBClient(
-    url=url,
-    token=token,
-    org=org
-)
-write_api = client.write_api(write_options=SYNCHRONOUS)
-
-def simulate_valkyrie():
+def simulate_valkyrie(write_api):
 
     def parse_date(datestr):
         s = (datestr[:-4]+datestr[-3:])
@@ -99,4 +84,19 @@ def simulate_valkyrie():
             time.sleep(delta.total_seconds())
 
 if __name__ == '__main__':
-    simulate_valkyrie()
+
+    load_dotenv()
+
+    url = os.getenv('INFLUX_URL')
+    bucket = os.getenv('INFLUX_BUCKET')
+    org = os.getenv('INFLUX_ORG')
+    token = os.getenv('INFLUX_TOKEN')
+    measurement = 'sitl'
+
+    client = influxdb_client.InfluxDBClient(
+        url=url,
+        token=token,
+        org=org
+    )
+
+    simulate_valkyrie(client.write_api(write_options=SYNCHRONOUS))
