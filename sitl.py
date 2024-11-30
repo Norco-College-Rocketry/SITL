38267cf2a2894c20ecea09ebfbe940ce4074121a
ahthae <122731557+ahthae@users.noreply.github.com>
2024-11-30 00:19:58 -0800

    Make Influx calls async

--- sitl.py
+++ sitl.py
@@ -2,89 +2,82 @@ import csv
 from datetime import datetime, UTC
 import itertools
 import json
-import paho.mqtt.publish as publish
+import paho.mqtt.client as mqtt
 import time
 import os
-import influxdb_client
-from influxdb_client.client.write_api import SYNCHRONOUS
+from influxdb_client import Point
+from influxdb_client.client.influxdb_client_async import InfluxDBClientAsync
 from dotenv import load_dotenv
+import asyncio
 
-def simulate_valkyrie(write_api):
-
+async def simulate_valkyrie(mqttc, write_api, bucket, org, measurement):
     def parse_date(datestr):
         s = (datestr[:-4]+datestr[-3:])
         return datetime.strptime(s, "%H:%M:%S:%f")
 
-    points = [influxdb_client.Point(measurement).tag('location', 'injector'),
-              influxdb_client.Point(measurement).tag('location', 'vent'),
-              influxdb_client.Point(measurement).tag('location', 'chamber'),
-              influxdb_client.Point(measurement),
-              influxdb_client.Point(measurement).tag('location', 'injector'),
-              influxdb_client.Point(measurement).tag('location', 'tank'),
-              influxdb_client.Point(measurement).tag('location', 'feed'),
-              influxdb_client.Point(measurement).tag('location', '1'),
-              influxdb_client.Point(measurement).tag('location', '2')]
+    metadata = [{'location':'injector', 'field':'temperature', 'topic':'telemetry/injector/temperature', 'unit':'F'},
+                {'location':'vent',     'field':'temperature', 'topic':'telemetry/vent/temperature',     'unit':'F'},
+                {'location':'chamber',  'field':'temperature', 'topic':'telemetry/chamber/temperature',  'unit':'F'},
+                {'location':'injector', 'field':'pressure',    'topic':'telemetry/injector/pressure',    'unit':'psi'},
+                {'location':'tank',     'field':'pressure',    'topic':'telemetry/tank/pressure',        'unit':'psi'},
+                {'location':'feed',     'field':'pressure',    'topic':'telemetry/feed/pressure',        'unit':'psi'},
+                {'location':'1',        'field':'weight',      'topic':'telemetry/weight/1',             'unit':'kg'},
+                {'location':'2',        'field':'weight',      'topic':'telemetry/weight/2',             'unit':'kg'}]
 
-    fields = ['temperature', 
-              'temperature',
-              'temperature',
-              '',
-              'pressure',
-              'pressure',
-              'pressure',
-              'weight',
-              'weight']
-
-    topics = [['telemetry/injector/temperature', 'F'],
-              ['telemetry/vent/temperature', 'F'],
-              ['telemetry/chamber/temperature', 'F'],
-              ['', ''],
-              ['telemetry/injector/pressure', 'psi'],
-              ['telemetry/tank/pressure', 'psi'],
-              ['telemetry/feed/pressure', 'psi'],
-              ['telemetry/weight/1', 'kg'],
-              ['telemetry/weight/2', 'kg']]
-
-    source_dir = os.path.dirname(os.path.realpath(__file__))
+    source_dir = os.path.dirname(os.path.realpath(__file__)) # Find data file relative to script directory
     filename = os.path.join(source_dir, 'data', 'Datafile_1.csv')
+    tasks = set()
     with open(filename) as data_file:
         reader = csv.reader(data_file)
-        next(reader)
+        header = next(reader)
+        elide = header.index(' N/A')
         sim_ts = datetime.now(UTC)
         for (row, next_row) in itertools.pairwise(reader):
+            del row[elide] # Remove unused column
             ts = parse_date(row[0])
             print(sim_ts)
+
             msgs = []
+            points = []
             for i in range(0, len(row)-1):
-                topic = topics[i][0]
-                units = topics[i][1]
-                if topic != '':
-                    value = float(row[i+1])
-                    if (topic[:-2] == 'telemetry/weight'):
-                        # Convert to kg
-                        value /= 1000
+                topic = metadata[i]['topic']
+                unit = metadata[i]['unit']
+                value = float(row[i+1])
+            
+                if (metadata[i]['field'] == 'weight'):
+                    # Convert to kg
+                    value /= 1000
 
-                    # Write to InfluxDB
-                    p = points[i].field(fields[i], value).time(sim_ts)
-                    write_api.write(bucket=bucket, org=org, record=p)
+                # Create Influx point
+                points.append(Point(measurement).tag('location', metadata[i]['location']).field(metadata[i]['field'], value).time(sim_ts))
 
-                    # Create MQTT message
-                    msgs.append({
-                        'topic': topic,
-                        'payload': json.dumps({
-                            'timestamp': sim_ts.timestamp()*1000, 
-                            'value': value,
-                            'unit': units 
-                            })
+                # Create MQTT message
+                msgs.append({
+                    'topic': topic,
+                    'payload': json.dumps({
+                        'timestamp': sim_ts.timestamp()*1000, 
+                        'value': value,
+                        'unit': unit
                         })
+                    })
 
-            publish.multiple(msgs)
+            # Write to InfluxDB
+            task = asyncio.create_task(write_api.write(bucket=bucket, record=points))
+            tasks.add(task)
+            task.add_done_callback(tasks.discard)
+            # Publish to MQTT
+            for msg in msgs:
+                mqttc.publish(msg['topic'], msg['payload'])
+
+            # Progress simulation time
             delta = parse_date(next_row[0])-ts
             sim_ts += delta
             time.sleep(delta.total_seconds())
 
-if __name__ == '__main__':
+    while len(tasks) != 0:
+        await tasks.pop()
 
+async def main():
     load_dotenv()
 
     url = os.getenv('INFLUX_URL')
@@ -93,10 +86,11 @@ if __name__ == '__main__':
     token = os.getenv('INFLUX_TOKEN')
     measurement = 'sitl'
 
-    client = influxdb_client.InfluxDBClient(
-        url=url,
-        token=token,
-        org=org
-    )
+    mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+    mqttc.connect('localhost')
 
-    simulate_valkyrie(client.write_api(write_options=SYNCHRONOUS))
+    async with InfluxDBClientAsync(url=url, token=token, org=org) as client:
+        await simulate_valkyrie(mqttc, client.write_api(), bucket, org, measurement)
+
+if __name__ == '__main__':
+    asyncio.run(main())
